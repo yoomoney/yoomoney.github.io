fd3c98be0317dadf2c8eb57433705928bc3a3ba9
Leonid Motorin <motorin@yoomoney.ru>
2021-10-21 19:46:17 +0300

    Fixes image shrink

--- src/components/PageSections/Hero/index.tsx
+++ src/components/PageSections/Hero/index.tsx
@@ -6,30 +6,35 @@ import {Text} from '../../Text';
 import {Title} from '../../Title';
 import {Section} from '../../Section';
 
-const StyledSection = styled(Section)`
+const StyledContainer = styled(Section)`
 	display: flex;
 	flex-wrap: wrap-reverse;
 `;
 
+const StyledImageSection = styled(Section)`
+	flex-grow: 1;
+	flex-shrink: 2;
+	flex-basis: 350px;
+`;
+
 export const Hero = () => {
 	return (
-		<StyledSection indent='xxxl'>
+		<StyledContainer indent='xxxl'>
 			<Section size='l'>
 				<Title size='l'>Открываемся для&nbsp;разработчиков</Title>
 				<Text size='l'>
 					Open Source помогает нам делать успешные и полезные продукты, которые меняют финтех-разработку. Поэтому мы делимся кодом с вами.
 				</Text>
 			</Section>
-			<Section size='m'>
+			<StyledImageSection size='m'>
 				<StaticImage
-					width={550}
 					quality={95}
 					placeholder='none'
 					alt='YooMoney Open Source'
 					style={{overflow: 'visible'}}
 					src='../../../assets/images/source.png'
 				/>
-			</Section>
-		</StyledSection>
+			</StyledImageSection>
+		</StyledContainer>
 	);
 };
