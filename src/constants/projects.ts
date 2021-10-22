6c137467f9d3597735a5a2c122abecb920471e83
Oleg Kandaurov <kandaurov@yoomoney.ru>
2021-10-22 10:52:11 +0300

    Добавил ссылки на SDK и CMS

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -1,26 +1,26 @@
 export const ProjectList = [
 	{
-		name: 'yookassa-payments-swift',
-		description: 'Библиотека позволяет реализовать прием платежей в мобильных приложениях на iOS и работает как расширение YooMoney API.'
-	},
-	{
-		name: 'yookassa-android-sdk',
-		description: 'Библиотека позволяет реализовать прием платежей в мобильных приложениях на Android и работает как расширение YooMoney API.'
-	},
-	{
-		name: 'yookassa-sdk-php',
-		description: 'PHP SDK для интеграции с YooKassa Payments API.'
-	},
-	{
-		name: 'mobile-analytics-chart-swift',
-		description: 'Библиотека позволяет внедрять диаграммы в мобильные приложения.'
-	},
-	{
 		name: 'db-queue ',
-		description: 'Реализация рабочей очереди поверх Java и базы данных.'
+		description: 'Реализация очереди поверх Java и реляционных баз данных.'
 	},
 	{
 		name: 'mobile-analytics-chart-swift',
-		description: 'DSL для создания дашбордов в Grafana'
+		description: 'Построение диаграмм в приложениях на iOS.'
+	},
+	{
+		name: 'grafana-dashboard-dsl',
+		description: 'DSL для создания дашбордов Grafana.'
+	},
+	{
+		name: 'grafana-dashboard-plugin',
+		description: 'Gradle плагин для загрузки дашбордов в Grafana.'
+	},
+	{
+		name: 'moira-kotlin-dsl',
+		description: 'DSL для создания триггеров Moira.'
+	},
+	{
+		name: 'moira-trigger-plugin',
+		description: 'Gradle плагин для загрузки дашбордов в Moira.'
 	}
 ];
